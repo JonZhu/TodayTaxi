58b6fae50d80311a9ca57f87fc698119bfb5f2f3
zhujun <juncke@163.com>
2016-12-20 22:15:10 +0800

    设置map类型为MAP_TYPE_NORMAL

--- android/map/src/main/java/com/todaytaxi/map/ReactMapViewManager.java
+++ android/map/src/main/java/com/todaytaxi/map/ReactMapViewManager.java
@@ -1,5 +1,6 @@
 package com.todaytaxi.map;
 
+import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapView;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
@@ -7,7 +8,7 @@ import com.facebook.react.uimanager.ThemedReactContext;
 /**
  * React地图ui组件
  *
- * Created by Administrator on 2016/12/17 0017.
+ * Created by zhujun on 2016/12/17 0017.
  */
 
 public class ReactMapViewManager extends SimpleViewManager<MapView> {
@@ -19,7 +20,9 @@ public class ReactMapViewManager extends SimpleViewManager<MapView> {
     }
 
     public MapView createViewInstance(ThemedReactContext context) {
-        return new MapView(context);
+        MapView mapView = new MapView(context);
+        mapView.getMap().setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        return mapView;
     }
 
 }
