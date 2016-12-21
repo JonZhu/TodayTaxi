6e4456eec66aaacd99ab354e5cbd661461c95a25
zhujun <jun.zhu@eayun.com>
2016-12-21 11:06:08 +0800

    升级react-native配置

--- index.ios.js
+++ index.ios.js
@@ -12,7 +12,7 @@ import {
   View
 } from 'react-native';
 
-export default class RNClient extends Component {
+export default class TodayTaxi extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -50,4 +50,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('RNClient', () => RNClient);
+AppRegistry.registerComponent('TodayTaxi', () => TodayTaxi);
