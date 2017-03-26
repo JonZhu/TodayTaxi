e97ddf7347c0ca09ce59ae87a7897fed32c10a37
zhujun <juncke@163.com>
2017-03-26 11:14:41 +0800

    设置空车

--- android/map/src/main/java/com/todaytaxi/map/ReactMapViewManager.java
+++ android/map/src/main/java/com/todaytaxi/map/ReactMapViewManager.java
@@ -12,6 +12,7 @@ import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.MapStatus;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.Marker;
 import com.baidu.mapapi.map.MarkerOptions;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.map.OverlayOptions;
@@ -22,6 +23,9 @@ import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -130,29 +134,48 @@ public class ReactMapViewManager extends SimpleViewManager<MapView> {
      * 设置当前空闲的Taxi
      *
      * @param view
-     * @param taxies
+     * @param taxies [{id, lat, lng}]
      */
     @ReactProp(name = "freeTaxies")
     public void setFreeTaxies(MapView view, ReadableArray taxies) {
+        Map<String, Marker> oldTaxiMap = (Map)MapViewExtendData.getData(view, "taxiMarkerMap");
+        Map<String, Marker> newTaxiMap = new HashMap<>();
+
         if (taxies != null && taxies.size() > 0) {
             for (int i = 0; i < taxies.size(); i++) {
                 ReadableMap map = taxies.getMap(i);
+                String taxiId = map.getString("id");
                 //定义Maker坐标点
-                LatLng point = new LatLng(39.963175, 116.400244);
-                //构建Marker图标
-                BitmapDescriptor bitmap = BitmapDescriptorFactory
-                        .fromResource(R.drawable.taxi_top_yellow);
-                //构建MarkerOption，用于在地图上添加Marker
-                OverlayOptions option = new MarkerOptions()
-                        .position(point)
-                        .icon(bitmap);
-                //在地图上添加Marker，并显示
-                view.getMap().addOverlay(option);
+                LatLng point = new LatLng(map.getDouble("lat"), map.getDouble("lng"));
+                Marker marker = null;
+                if (oldTaxiMap != null && oldTaxiMap.containsKey(taxiId)) {
+                    // marker已经存在
+                    marker = oldTaxiMap.get(taxiId);
+                    marker.setPosition(point);
+                    oldTaxiMap.remove(taxiId);
+                } else {
+                    //构建Marker图标
+                    BitmapDescriptor bitmap = BitmapDescriptorFactory.fromResource(R.drawable.taxi_top_yellow);
+                    //构建MarkerOption，用于在地图上添加Marker
+                    OverlayOptions option = new MarkerOptions()
+                            .position(point)
+                            .icon(bitmap);
+                    //在地图上添加Marker，并显示
+                    marker = (Marker)view.getMap().addOverlay(option);
+                }
+
+                newTaxiMap.put(taxiId, marker);
             }
-        } else {
-            // 清除所有free taxi
         }
 
+        // 清除marker
+        if (oldTaxiMap != null && !oldTaxiMap.isEmpty()) {
+            for (Marker marker : oldTaxiMap.values()) {
+                marker.remove();
+            }
+        }
+
+        MapViewExtendData.setData(view, "taxiMarkerMap", newTaxiMap);
     }
 
     /**
