deaf18e384f34e987f56d75b60829af4aff39f2e
zhujun <jun.zhu@eayun.com>
2017-04-07 16:41:50 +0800

    测试高德导航组件

--- component/test/MapViewTest.js
+++ component/test/MapViewTest.js
@@ -6,15 +6,17 @@
  */
 
 import React, { Component } from 'react';
-import { View, Button } from 'react-native';
-import MapView from '../../native/MapView';
+import { View, Button, ToastAndroid } from 'react-native';
+import MapNaviView from '../../native/MapNaviView';
+import NaviModule from '../../native/NaviModule';
 
 class MapViewTest extends Component {
 
     constructor() {
         super();
-
+        this.state = {showNaviView:false};
         this._testNavi = this._testNavi.bind(this);
+        this._testStopNavi = this._testStopNavi.bind(this);
     }
 
     componentDidMount() {
@@ -25,14 +27,30 @@ class MapViewTest extends Component {
     _testNavi() {
         // 104.063244,30.544046 天府五街
         // 104.077239,30.555243 软件园A区
-        this._mapView.launchNavi([{lng:104.063244, lat:30.544046, name:'天府五街'}, {lng:104.077239, lat:30.555243, name:'软件园A区'}]);
+        NaviModule.startNavi([{lng:104.063244, lat:30.544046, name:'天府五街'}, {lng:104.077239, lat:30.555243, name:'软件园A区'}]).then((value)=>{
+            ToastAndroid.show('导航成功', ToastAndroid.LONG);
+            this.setState({showNaviView:true});
+        }).catch((reason)=>{
+            ToastAndroid.show(reason, ToastAndroid.LONG);
+        });
+    }
+
+    _testStopNavi() {
+        NaviModule.stopNavi();
+        this.setState({showNaviView:false});
     }
 
     render() {
         return (
             <View style={{flex:1}}>
-                <Button title='导航' onPress={this._testNavi}/>
-                <MapView ref={(value)=>{this._mapView=value}} style={{flex:1}}/>
+                <View style={{flexDirection:'row'}}>
+                    <Button title='开始导航' onPress={this._testNavi}/>
+                    <Button title='停止导航' onPress={this._testStopNavi}/>
+                </View>
+
+                {this.state.showNaviView &&
+                <MapNaviView style={{flex:1, width:'100%'}}/>
+                }
             </View>
         );
     }
