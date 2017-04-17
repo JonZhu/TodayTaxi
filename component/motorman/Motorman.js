d38f9aa3a014a46f2d74b39e946e4f7eab7fa969
zhujun <jun.zhu@eayun.com>
2017-04-17 17:37:56 +0800

    司机端：接受行程后，增加导航和后续操作

--- component/motorman/Motorman.js
+++ component/motorman/Motorman.js
@@ -10,6 +10,8 @@ import ToolBar from '../calltaxi/ToolBar';
 import Map from '../calltaxi/Map';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import MapModule from '../../native/MapModule';
+import NaviModule, { addNaviArriveWayPointListener } from '../../native/NaviModule';
+import MapNaviView from '../../native/MapNaviView';
 import rest from '../api/rest';
 
 class Motorman extends Component {
@@ -25,6 +27,10 @@ class Motorman extends Component {
         this._stopPushRouteLoc = this._stopPushRouteLoc.bind(this);
         this._acceptRoute = this._acceptRoute.bind(this);
         this._rejectRoute = this._rejectRoute.bind(this);
+        this._arriveRouteFrom = this._arriveRouteFrom.bind(this);
+        this._passengerGetOn = this._passengerGetOn.bind(this);
+        this._completeRoute = this._completeRoute.bind(this);
+        this._cancelRoute = this._cancelRoute.bind(this);
     }
 
     componentDidMount() {
@@ -34,6 +40,10 @@ class Motorman extends Component {
     componentWillUnmount() {
         this._stopPushFreeLoc();
         this._stopPushRouteLoc();
+        if (this._naviArriveWayPointListener) {
+            this._naviArriveWayPointListener.stop();
+            this._naviArriveWayPointListener = null;
+        }
     }
 
 
@@ -51,14 +61,15 @@ class Motorman extends Component {
     _startPushFreeLoc() {
         this._stopPushFreeLoc();
 
-        function pushFun() {
+        var pushFun = ()=>{
             // 定位
             MapModule.location().then((result)=>{
                 // 定位返回
+                this._currentLoc = {lat:result.lat, lng:result.lng, 
+                    address:result.address, direction:result.direction, speed:result.speed}; // 存储当前位置
 
                 // 上传位置到服务器
-                return rest('/taxi/pushFreeLoc.do', {lat:result.lat, lng:result.lng, 
-                    address:result.address, direction:result.direction, speed:result.speed});
+                return rest('/taxi/pushFreeLoc.do', this._currentLoc);
             }).then((result)=>{
                 // 上传空车位置返回
 
@@ -74,7 +85,7 @@ class Motorman extends Component {
             });
         };
 
-        this._pushFreeLocTimer = setInterval(pushFun.bind(this), 1000);
+        this._pushFreeLocTimer = setInterval(pushFun, 1000);
     }
 
     // 接受预分配的行程
@@ -82,10 +93,8 @@ class Motorman extends Component {
         var route = this.state.preAllocateRoute;
         rest('/taxi/acceptRoute.do', {passengerId: route.passengerId}).then((result)=>{
             if (result.code === 0) {
-                // 关闭接单窗口
-                this.setState({routeId:result.payload}); // 存储routeId
-                this._startNavi(route); // 开始导航
-                this._startPushRouteLoc(); // 开始上报行程位置
+                this.setState({routeId:result.payload, showAccept:false}); // 存储routeId, 关闭接单窗口
+                this._startNavi(); // 开始导航
             } else {
                 ToastAndroid.show(result.message, ToastAndroid.LONG);
             }
@@ -131,8 +140,82 @@ class Motorman extends Component {
     }
 
     // 开始导航
-    _startNavi(route) {
+    _startNavi() {
+        var route = this.state.preAllocateRoute;
+        var cLoc = this._currentLoc;
+        var pointList = [{lng:cLoc.lng, lat:cLoc.lat}, {lng:route.from.lng, lat:route.from.lat}, 
+            {lng:route.to.lng, lat:route.to.lat}]; // 从当前位置导航，途经点为行程开始位置
+        NaviModule.startNavi(pointList).then(()=>{
+            // 导航成功
+            this.setState({showNaviView: true});
+            this._startPushRouteLoc(); // 开始上报行程位置
+            this._naviArriveWayPointListener = addNaviArriveWayPointListener(()=>{
+                // 到达行程起点, 用于自动设置 到达指定位置
+                // TODO
+            });
+        }).catch((reason)=>{
+            // 失败
+            ToastAndroid.show('导航失败:' + reason, ToastAndroid.LONG);
+            // 
+        });
+    }
 
+    // 到达行程开始位置
+    _arriveRouteFrom() {
+        rest('/taxi/arriveRouteFrom.do', {routeId: this.state.preAllocateRoute.routeId}).then((result)=>{
+            if (result.code === 0) {
+                // 成功
+                ToastAndroid.show('操作成功', ToastAndroid.LONG);
+            } else {
+                ToastAndroid.show(result.message, ToastAndroid.LONG);
+            }
+        }).catch((reason)=>{
+            ToastAndroid.show(reason, ToastAndroid.LONG);
+        });
+    }
+
+
+    
+    // 乘客上车
+    _passengerGetOn() {
+        rest('/taxi/passengerGetOn.do', {routeId: this.state.preAllocateRoute.routeId}).then((result)=>{
+            if (result.code === 0) {
+                // 成功
+                ToastAndroid.show('操作成功', ToastAndroid.LONG);
+            } else {
+                ToastAndroid.show(result.message, ToastAndroid.LONG);
+            }
+        }).catch((reason)=>{
+            ToastAndroid.show(reason, ToastAndroid.LONG);
+        });
+    }
+
+    // 完成行程
+    _completeRoute() {
+        rest('/taxi/completeRoute.do', {routeId: this.state.preAllocateRoute.routeId}).then((result)=>{
+            if (result.code === 0) {
+                // 成功
+                ToastAndroid.show('操作成功', ToastAndroid.LONG);
+            } else {
+                ToastAndroid.show(result.message, ToastAndroid.LONG);
+            }
+        }).catch((reason)=>{
+            ToastAndroid.show(reason, ToastAndroid.LONG);
+        });
+    }
+
+    // 取消行程
+    _cancelRoute() {
+        rest('/taxi/cancelRoute.do', {routeId: this.state.preAllocateRoute.routeId}).then((result)=>{
+            if (result.code === 0) {
+                // 成功
+                ToastAndroid.show('操作成功', ToastAndroid.LONG);
+            } else {
+                ToastAndroid.show(result.message, ToastAndroid.LONG);
+            }
+        }).catch((reason)=>{
+            ToastAndroid.show(reason, ToastAndroid.LONG);
+        });
     }
 
 
@@ -152,18 +235,30 @@ class Motorman extends Component {
                         <View style={style.container}>
                             <View style={[style.row, style.fromRow]}>
                                 <Icon name='circle' style={style.fromIcon}/>
-                                <Text style={style.text}>{preAllocateRoute.fromAddress}</Text>
+                                <Text style={style.text}>{preAllocateRoute.from.address}</Text>
                             </View>
                         
                             <View style={[style.row, style.goRow]}>
                                 <Icon name='circle' style={style.goIcon}/>
-                                <Text style={style.text}>{preAllocateRoute.toAddress}</Text>
+                                <Text style={style.text}>{preAllocateRoute.to.address}</Text>
                             </View>
                     
                         </View>
                         <View style={{flexDirection:'row', justifyContent:'space-around'}}>
                             <Button title='　拒　绝　' onPress={this._rejectRoute}/>
                             <Button title='　接　受　' onPress={this._acceptRoute}/>
+                        </View>
+                    </View>
+                    }
+
+                    {this.state.showNaviView &&
+                    <View style={{position:'absolute', top:0, bottom:0, left:0, right:0}}>
+                        <MapNaviView style={{position:'absolute', top:0, bottom:0, left:0, right:0}}/>
+                        <View style={{position:'absolute', bottom:0, left:0, right:0}}>
+                            <Button title='到达指定位置' onPress={this._arriveRouteFrom}/>
+                            <Button title='乘客上车' onPress={this._passengerGetOn}/>
+                            <Button title='完成' onPress={this._completeRoute}/>
+                            <Button title='取消' onPress={this._cancelRoute}/>
                         </View>
                     </View>
                     }
