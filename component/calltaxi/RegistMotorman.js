d018c0f633eb3b8e0e8d727a4bb5ba30846c4907
zhujun <jun.zhu@eayun.com>
2017-04-26 14:34:17 +0800

    客户端：注册车主增加 品牌、型号、颜色 参数

--- component/calltaxi/RegistMotorman.js
+++ component/calltaxi/RegistMotorman.js
@@ -3,7 +3,7 @@
  */
 
 import React, { Component } from 'react';
-import { View, Text, Button, TextInput, TouchableWithoutFeedback, ToastAndroid } from 'react-native';
+import { View, Text, Button, TextInput, TouchableWithoutFeedback, ToastAndroid, ScrollView } from 'react-native';
 import Header from '../Header';
 import rest from '../api/rest';
 import { pageBack } from '../util/back';
@@ -21,7 +21,10 @@ class RegistMotorman extends Component {
             taxiNumber: this._taxiNumber,
             chassisNumber: this._chassisNumber,
             engineNo: this._engineNo,
-            ownerName: this._ownerName
+            ownerName: this._ownerName,
+            brand: this._brand,
+            model: this._model,
+            color: this._color
         };
 
         if (!param.ownerName) {
@@ -60,7 +63,7 @@ class RegistMotorman extends Component {
         return (
             <View style={{flex:1, backgroundColor:'#fff'}}>
                 <Header title='注册成为车主' icon='back' iconOnPress={pageBack.bind(this)}/>
-                <View style={{flex:1, padding:10}}>
+                <ScrollView style={{flex:1, padding:10}}>
                     <View style={{borderWidth:1, borderRadius:3, borderColor:'rgb(205,205,211)', marginBottom:10}}>
                         <View style={{flexDirection:'row', alignItems:'center', borderBottomWidth:1, 
                             borderBottomColor:'rgb(205,205,211)', paddingLeft:10, height:50}}>
@@ -80,16 +83,34 @@ class RegistMotorman extends Component {
                             <TextInput style={{flex:1}} placeholder='请输入车架号' underlineColorAndroid='transparent' 
                                 onChangeText={(text)=>{this._chassisNumber = text}}/>
                         </View>
-                        <View style={{flexDirection:'row', alignItems:'center', paddingLeft:10, height:50}}>
+                        <View style={{flexDirection:'row', alignItems:'center', borderBottomWidth:1, 
+                            borderBottomColor:'rgb(205,205,211)', paddingLeft:10, height:50}}>
                             <Text style={{fontSize:18}}>发动机号：</Text>
                             <TextInput style={{flex:1}} placeholder='请输入发动机号' underlineColorAndroid='transparent' 
                                 onChangeText={(text)=>{this._engineNo = text}}/>
+                        </View>
+                        <View style={{flexDirection:'row', alignItems:'center', borderBottomWidth:1, 
+                            borderBottomColor:'rgb(205,205,211)', paddingLeft:10, height:50}}>
+                            <Text style={{fontSize:18}}>品牌：</Text>
+                            <TextInput style={{flex:1}} placeholder='如：大众' underlineColorAndroid='transparent'
+                                onChangeText={(text)=>{this._brand = text}}/>
+                        </View>
+                        <View style={{flexDirection:'row', alignItems:'center', borderBottomWidth:1, 
+                            borderBottomColor:'rgb(205,205,211)', paddingLeft:10, height:50}}>
+                            <Text style={{fontSize:18}}>型号：</Text>
+                            <TextInput style={{flex:1}} placeholder='如：2015款手动风尚' underlineColorAndroid='transparent'
+                                onChangeText={(text)=>{this._model = text}}/>
+                        </View>
+                        <View style={{flexDirection:'row', alignItems:'center', paddingLeft:10, height:50}}>
+                            <Text style={{fontSize:18}}>颜色：</Text>
+                            <TextInput style={{flex:1}} placeholder='如：白色' underlineColorAndroid='transparent'
+                                onChangeText={(text)=>{this._color = text}}/>
                         </View>
                     </View>
 
                     <Button title='注册' onPress={this._regist}></Button>
 
-                </View>
+                </ScrollView>
             </View>
         );
     }
