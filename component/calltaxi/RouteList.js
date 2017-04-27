9f3bbf03d1194015e8dc439f6a11b23dee0aa2d8
zhujun <jun.zhu@eayun.com>
2017-04-27 15:38:28 +0800

    行程列表，显示服务器真实数据 ， 实现下拉刷新、上拉加载下一页数据

--- component/calltaxi/RouteList.js
+++ component/calltaxi/RouteList.js
@@ -9,32 +9,70 @@ import { View, ToastAndroid, Text, TouchableWithoutFeedback, FlatList } from 're
 import Header from '../Header';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import rest from '../api/rest';
+import { pageBack } from '../util/back';
 
 class RouteList extends Component {
     constructor() {
         super();
-
-        // 添加测试数据
-        var routeList = [];
-        for (var i=0; i < 20; i++) {
-            routeList.push({key:i, time:1493102919000, from:'天府广场', to:'双流', status:1});
-        }
-        this.state = {routeList: routeList};
+        this.state = {routeList: [], refreshing:false};
     }
 
     componentDidMount() {
         this._queryNextPageRoute();
     }
 
-    // 查询下一页行程数据
-    _queryNextPageRoute = ()=>{
-        rest('/route/getPassengerRouteList.do', {}).then();
+    // 下位刷新列表
+    _refreshRouteList = ()=>{
+        this.state.routeList = []; // 清空原来数据
+        this._queryNextPageRoute();
     }
 
+    // 上拉, 查询下一页行程数据
+    _queryNextPageRoute = ()=>{
+        var param = {pageSize: 20};
+        var routeList = this.state.routeList;
+        if (routeList != null && routeList.length > 0) {
+            param.timeBefore = routeList[routeList.length - 1].startTime; // 取上一页最后一条数据的时间
+        }
+
+        rest('/route/getPassengerRouteList.do', param).then((result)=>{
+            if (result.code === 0) {
+                routeList = this._concatRouteList(routeList, result.payload);
+                this.setState({routeList: routeList, refreshing:false});
+                // this.setState({refreshing:false});
+            } else {
+                this.setState({refreshing:false});
+                ToastAndroid.show(result.message, ToastAndroid.LONG);
+            }
+        }).catch((reason)=>{
+            this.setState({refreshing:false});
+            ToastAndroid.show(reason, ToastAndroid.LONG);
+        });
+        this.setState({refreshing:true});
+    }
+
+    // 合并行程数据
+    _concatRouteList = (oldList, newList)=>{
+        if (oldList == null) {
+            oldList = [];
+        }
+
+        if (newList != null && newList.length > 0) {
+            for (var i = 0; i < newList.length; i++) {
+                var route = newList[i];
+                route.key = route.id; // 根据flatlist，设置key字段
+                oldList.push(route);
+            }
+        }
+
+        return oldList;
+
+    }
+
+    // 毫秒转时间字符串
     _toTimeStr = (time)=>{
         try {
             var date = new Date(time);
-            // date.setMilliseconds(time);
             return date.getFullYear() + '年' + date.getMonth() + '月' + date.getDate() + '日 ' 
                 + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
         } catch (error) {
@@ -42,12 +80,28 @@ class RouteList extends Component {
         }
     }
 
+    // 状态映射
+    _statusMap = {
+        11: '分配司机',
+        20: '司机取消',
+        21: '乘客取消',
+        30: '司机已到达',
+        31: '已上车',
+        40: '完成'
+    }
+
+    // 转换状态
+    _toConvertStatus = (status)=>{
+        var s = this._statusMap[status];
+        return s ? s : "--";
+    }
+
     render() {
         return (
             <View style={{flex:1, backgroundColor:'#fff'}}>
-                <Header title='我的行程' icon='back'/>
+                <Header title='我的行程' icon='back' iconOnPress={pageBack.bind(this)}/>
                 <View style={{flex:1}}>
-                    <FlatList data={this.state.routeList}
+                    <FlatList data={this.state.routeList} onEndReached={this._queryNextPageRoute} refreshing={this.state.refreshing} onRefresh={this._refreshRouteList}
                         renderItem={(row)=>{
                             var item = row.item;
                             // row.index
@@ -55,16 +109,16 @@ class RouteList extends Component {
                                 <View style={{padding: 5, margin:10, marginTop:5, marginBottom:5, borderWidth:1, 
                                     borderColor:'rgb(219,219,219)', borderRadius:3}}>
                                     <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-                                        <Text style={{fontWeight:'bold'}}>{this._toTimeStr(item.time)}</Text>
-                                        <Text>已完成</Text>
+                                        <Text style={{fontWeight:'bold'}}>{this._toTimeStr(item.startTime)}</Text>
+                                        <Text>{this._toConvertStatus(item.status)}</Text>
                                     </View>
                                     <View style={{flexDirection:'row', alignItems:'center'}}>
                                         <Icon name='circle' style={{color:'rgb(47,168,32)'}}/>
-                                        <Text style={{padding:3, paddingLeft:10}}>{item.from}</Text>
+                                        <Text style={{padding:3, paddingLeft:10}}>{item.fromAddress}</Text>
                                     </View>
                                     <View style={{flexDirection:'row', alignItems:'center'}}>
                                         <Icon name='circle' style={{color:'rgb(243,47,0)'}}/>
-                                        <Text style={{padding:3, paddingLeft:10}}>{item.to}</Text>
+                                        <Text style={{padding:3, paddingLeft:10}}>{item.toAddress}</Text>
                                     </View>
                                 </View>
                             )
