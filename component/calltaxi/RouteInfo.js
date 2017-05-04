0767bdd84627935564b2acf9a1b2db9e599648f9
zhujun <juncke@163.com>
2017-05-04 21:18:41 +0800

    行程详情样式

--- component/calltaxi/RouteInfo.js
+++ component/calltaxi/RouteInfo.js
@@ -18,7 +18,9 @@ class RouteInfo extends Component {
             <View style={{flex:1, backgroundColor:'#fff'}}>
                 <Header title='行程' icon='back' iconOnPress={pageBack.bind(this)}/>
                 <View style={{flex:1}}>
-                    <View style={{borderLeftWidth:1, marginLeft:20, paddingLeft:-3}}>
+                    <View style={{paddingLeft:20, borderBottomWidth:1, borderBottomColor:'#f5f5f5'}}>
+                        <View style={{position:'absolute', borderLeftWidth:1, borderLeftColor:'#f5f5f5', top:10, bottom:10, left:25}}/>
+
                         <View style={{flexDirection:'row', alignItems:'center'}}>
                             <Icon name='circle' style={{color:'rgb(47,168,32)'}}/>
                             <Text style={{padding:3, paddingLeft:10}}>双流机场</Text>
@@ -31,7 +33,11 @@ class RouteInfo extends Component {
 
                     <MapView style={{flex:1}}/>
 
-                    <View></View>
+                    <View style={{paddingLeft:20, paddingRight:20, borderTopWidth:1, borderTopColor:'#f5f5f5'}}>
+                        <Text>车牌：川A888888</Text>
+                        <Text>司机：张三</Text>
+                        <Text>行程总记：￥37.8元</Text>
+                    </View>
                 </View>
             </View>
         )
