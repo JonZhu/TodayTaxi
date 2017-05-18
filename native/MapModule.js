3bdfc1271a168c585345b3d0c160875d5c1a6adf
zhujun <jun.zhu@eayun.com>
2017-05-18 15:25:09 +0800

    js端：增加addLocChangedListener方法

--- native/MapModule.js
+++ native/MapModule.js
@@ -14,8 +14,31 @@
  * 在城市中搜索POI
  * void searchInCity(String city, String keyword, int dataLimit, final Promise promise) <br/>
  * 
- * 定位当前位置
+ * 定位当前位置, 只定位一次
  * void location(final Promise promise) <br/>
+ * 返回：{
+ *  lat,
+ *  lng,
+ *  direction,
+ *  speed,
+ *  city,
+ *  cityCode,
+ *  country,
+ *  address,
+ *  district,
+ *  describe
+ * }
+ * 
+ * 开始定位, 用于连续定位
+ * void startLocation(params) <br/>
+ * params: {
+ *  interval:发起定位请求的时间间隔，单位毫秒, 默认2000,
+ *  needAddress:是否需要详细地址, 默认true
+ * }
+ * 
+ * 
+ * 停止startLocation方法开启的定位
+ * void stopLocation()
  * 
  * 驾车路线规划
  * void drivingRoute(ReadableMap from, ReadableMap go, final Promise promise) <br/>
@@ -25,8 +48,19 @@
  * 
  */
 
-import { NativeModules } from 'react-native';
+import { NativeModules, NativeEventEmitter } from 'react-native';
 
 const MapModule = NativeModules.ReactBaiduMapModule;
 
+/**
+ * 添加定位改变监听器
+ * @param listener = (loc)=>{}, loc数据结构在location方法返回基础上，增加errorCode、errorInfo
+ *  当errorCode为0时，表示本次定位成功
+ * @return EmitterSubscription 用于清除监听器，使用remove()方法
+ */
+function addLocChangedListener(listener) {
+    return NativeEventEmitter.addListener('onLocChanged', listener);
+}
+
 export default MapModule;
+export {addLocChangedListener};
