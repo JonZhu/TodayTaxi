142ee2489a1ad668709e8aa60e22aa7820817017
zhujun <jun.zhu@eayun.com>
2017-06-06 11:13:37 +0800

    native导出SocialPackage

--- android/app/src/main/java/com/todaytaxi/MainApplication.java
+++ android/app/src/main/java/com/todaytaxi/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.todaytaxi.map.MapReactPackage;
+import com.todaytaxi.social.SocialReactPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new MapReactPackage()
+          new MapReactPackage(),
+          new SocialReactPackage()
       );
     }
   };
