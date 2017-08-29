d197aa030773ef3c1370e8c3acbac8376125e76b
zhujun <jun.zhu@eayun.com>
2017-08-29 16:40:16 +0800

    删除maintab reducer

--- redux/storeConfig.js
+++ redux/storeConfig.js
@@ -1,13 +1,11 @@
 import { createStore, combineReducers } from 'redux';
 import userinfo from './reducer/userinfo';
-import maintab from './reducer/maintab';
 import sideBar from './reducer/sideBar';
 import callTaxi from './reducer/callTaxi';
 
 function storeConfig() {
     return createStore(combineReducers({
         userinfo,
-        maintab,
         sideBar,
         callTaxi
     }));
